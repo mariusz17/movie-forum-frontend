ca69c09fb08830c09b1f6aa6b31f9b3e6a75d8d9
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2023-03-12 13:02:21 +0100

    add welcome text in english

--- src/translations/texts/en.ts
+++ src/translations/texts/en.ts
@@ -2,7 +2,14 @@ import { Texts } from './types';
 
 export const EN: Texts = {
   appName: 'Movie Forum',
-  welcome: 'Hello',
+  welcomeHeading: 'Hello!',
+  welcomeText:
+    'Welcome on movie and TV shows forum. If you do not have\
+    an account yet, I encourage you to create one, by clicking\
+    user icon in the top right corner. When you log in, you will\
+    gain access to forum discussions, ability to rate chosen films\
+    and adding them to favourite list. Always (no logging in required)\
+    you can use search tool that uses themoviedb.org API',
   forum: 'Forum',
   search: 'Search',
   searchPlaceholder: 'Search for movies and TV shows',
